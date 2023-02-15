7bff54cb6b68e194bd25a6ce0143da39a0e9233c
Anthony <acr1191@gmail.com>
2023-02-15 15:30:33 -0800

    create todo constructor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import './style.css';
+import Todo from './todo';
 
 const element = document.createElement('div');
 element.textContent = "Hello World";
 element.classList.add('hello');
 document.body.appendChild(element);
+
+const test = Todo('Pay bills', 'Pay credit card bills', '2/28/2023', 'high', 'check for any incorrect charges', 'monthly');
+console.log({test});
