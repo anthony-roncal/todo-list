1fa496f2d83bbac54b2873ba4190440bec208370
Anthony <acr1191@gmail.com>
2023-02-16 15:39:55 -0800

    add ability to delete todo items from list

--- src/index.js
+++ src/index.js
@@ -4,17 +4,15 @@ import Project from './project';
 import viewController from './viewController';
 
 let projects = [];
-const defaultProject = Project(0, "Default Project");
+const defaultProject = Project(0, "To-Do");
 projects.push(defaultProject);
-const view = viewController();
 
-// const testProject = Project(1, "Test Project");
-// const testTodo1 = Todo('1', 'Pay credit card bills', '2/28/2023', 'high', 'check for any incorrect charges');
-// const testTodo2 = Todo('2', 'Pay credit card bills', '2/28/2023', 'high', 'check for any incorrect charges');
-// const testTodo3 = Todo('3', 'Pay credit card bills', '2/28/2023', 'high', 'check for any incorrect charges');
-// testProject.addToDo(testTodo1);
-// testProject.addToDo(testTodo2);
-// testProject.addToDo(testTodo3);
-// console.log(testProject);
-// testProject.removeToDo(2);
-// console.log(testProject);
+const testTodo1 = Todo('Pay bills', 'Pay credit card bills', '2/28/2023', 'high', 'check for any incorrect charges');
+const testTodo2 = Todo('Grocery shopping', 'milk, eggs, cauliflower rice', '2/18/2023', 'high', 'check if we have coupons');
+const testTodo3 = Todo('Make doctor appointment', 'Make an appointment for a checkup', '3/01/2023', 'high', '');
+defaultProject.addToDo(testTodo1);
+defaultProject.addToDo(testTodo2);
+defaultProject.addToDo(testTodo3);
+
+const view = viewController(projects);
+
--- src/viewController.js
+++ src/viewController.js
@@ -1,14 +1,45 @@
-export default function viewController() {
+export default function viewController(projectsArray) {
     (function init() {
         const header = document.createElement('header');
         const sidebar = document.createElement('div');
+        const projectsList = document.createElement('ul');
         const content = document.createElement('div');
+        const todoList = document.createElement('ul');
+
+        header.textContent = 'Odin To-Do List';
 
         sidebar.classList.add('sidebar');
+        projectsArray.forEach(project => {
+            const projectListItem = document.createElement('li');
+            projectListItem.textContent = project.title;
+            projectsList.appendChild(projectListItem);
+        });
+        projectsList.classList.add('projects-ul');
+        sidebar.appendChild(projectsList);
+
         content.classList.add('content');
+        projectsArray[0].todos.forEach(todo => {
+            const todoListItem = document.createElement('li');
+            todoListItem.classList.add('todo-item');
+            todoListItem.textContent = todo.title;
+
+            const deleteBtn = document.createElement('button');
+            deleteBtn.classList.add('delete-todo');
+            deleteBtn.addEventListener('click', removeTodoItem);
+            
+            todoListItem.appendChild(deleteBtn);
+            todoList.appendChild(todoListItem);
+        });
+        todoList.classList.add('todo-ul');
+        content.appendChild(todoList);
 
         document.body.append(header, sidebar, content);
     })();
+
+    function removeTodoItem(e) {
+        projectsArray[0].removeToDo(Array.from(e.target.parentNode.parentNode.children).indexOf(e.target.parentNode));
+        e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+    }
 
     return {
         
